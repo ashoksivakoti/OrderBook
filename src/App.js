401d950db60f65cbe1729c794076ec7647a55d5a
ashoksivakoti <ashoksivakoti@gmail.com>
2023-09-02 15:25:56 +0530

    context and app are changed

--- src/App.js
+++ src/App.js
@@ -1,103 +1,141 @@
-import React, { useState } from 'react';
-import Binance from './components/Binance';
-import Kraken from './components/Kraken'; 
-import ConsolidatedOrderBook from './components/ConsolidatedOrderBook';
-import { useOrderBook } from './context/OrderBookContext';
-import { searchSuggestions } from './context/constant';
-
+import React, { useState } from "react";
+import Binance from "./components/Binance";
+import Kraken from "./components/Kraken";
+import ConsolidatedOrderBook from "./components/ConsolidatedOrderBook";
+import { useOrderBook } from "./context/OrderBookContext";
 
 const App = () => {
-  const{ searchQuery, setSearchQuery, binanceBuyers, krakenSellers, krakenBuyers} = useOrderBook()
-  const [selectedExchanges, setSelectedExchanges] = useState([]);
-  const [searching, setSearching] = useState(false);
+  // Destructuring values from the OrderBookContext
+  const {
+    searchQuery,
+    setSearchQuery,
+    intersectionValues,
+    binanceBuyers,
+    krakenBuyers,
+    toggleBinanceWebSocket,
+    toggleKrakenWebSocket,
+    selectedExchanges,
+    setSelectedExchanges,
+  } = useOrderBook();
 
+  const [searching, setSearching] = useState(false);
+  const [error, setError] = useState("");
+
+  // Handler for exchange checkbox changes
   const handleExchangeCheckboxChange = (event) => {
     const exchange = event.target.value;
     const isSelected = event.target.checked;
 
     if (isSelected) {
-      setSelectedExchanges((prevSelectedExchanges) => [...prevSelectedExchanges, exchange]);
+      // Add the selected exchange to the list
+      setSelectedExchanges((prevSelectedExchanges) => [
+        ...prevSelectedExchanges,
+        exchange,
+      ]);
+
+      // Open WebSocket for the selected exchange
+      if (exchange === "binance") {
+        toggleBinanceWebSocket(true);
+      } else if (exchange === "kraken") {
+        toggleKrakenWebSocket(true);
+      }
     } else {
+      // Remove the selected exchange from the list
       setSelectedExchanges((prevSelectedExchanges) =>
-        prevSelectedExchanges.filter((selectedExchange) => selectedExchange !== exchange)
+        prevSelectedExchanges.filter(
+          (selectedExchange) => selectedExchange !== exchange
+        )
       );
+
+      // Close WebSocket for the deselected exchange
+      if (exchange === "binance") {
+        toggleBinanceWebSocket(false);
+      } else if (exchange === "kraken") {
+        toggleKrakenWebSocket(false);
+      }
     }
   };
 
   const handleSearchClick = () => {
     setSearching(true);
-  };
 
-  const handleKeyPress = (event) => {
-
-    if (event.key === 'Enter') {
-      setSearching(true)
+    // Check if the entered value is in fetched intersection pairs
+    if (!intersectionValues.includes(searchQuery.toUpperCase())) {
+      setError("Trading pair not found");
+    } else {
+      setError("");
     }
   };
 
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSearchClick();
+    }
+  };
+  console.log("kraken : ", krakenBuyers);
+  console.log("binance : ", binanceBuyers);
+  console.log("intersection values", intersectionValues);
 
   return (
-    <>
-      <div className="min-h-screen bg-gray-100 py-10 px-4 sm:px-6 lg:px-8">
-        <h1 className="text-2xl font-semibold mb-4">Consolidated Order Book</h1>
-        <div className="mb-4">
-          <label>
-            <input
-              type="checkbox"
-              value="binance"
-              checked={selectedExchanges.includes('binance')}
-              onChange={handleExchangeCheckboxChange}
-              className='mr-2'
-            />
-            Binance
-          </label>
-          <label className="ml-4">
-            <input
-              type="checkbox"
-              value="kraken"
-              checked={selectedExchanges.includes('kraken')}
-              onChange={handleExchangeCheckboxChange}
-              className='mr-2'
-            />
-            Kraken
-          </label>
-        </div>
-        <div className="flex mb-4">
-        <input
-  type="text"
-  placeholder="Please select a trading pair"
-  value={searchQuery}
-  onChange={(e) => setSearchQuery(e.target.value)}
-  onKeyDown={handleKeyPress}
-  className="border rounded p-2 flex-grow mr-2"
-  list="symbolSuggestions"
-/>
-<datalist id="symbolSuggestions">
-  {searchSuggestions.map((suggestion, index) => (
-    <option key={index} value={suggestion} />
-  ))}
-</datalist>
-          <button
-            className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
-            onClick={handleSearchClick}
-          >
-            Search
-          </button>
-        </div>
-        {selectedExchanges.length === 0 && searching && (
-          <p className="text-center text-red-600">Please select an exchange to view the order book.</p>
-        )}
-        {selectedExchanges.includes('binance') && !selectedExchanges.includes('kraken') &&  (
-          <Binance />
-        )}
-        {selectedExchanges.includes('kraken') && !selectedExchanges.includes('binance') &&  (
-          <Kraken />
-        )}
-        {selectedExchanges.includes('binance') && selectedExchanges.includes('kraken') && (
-          <ConsolidatedOrderBook />
-        )}
+    <div className="min-h-screen bg-gray-100 py-10 px-4 sm:px-6 lg:px-8">
+      <h1 className="text-2xl font-semibold mb-4">Consolidated Order Book</h1>
+      <div className="mb-4">
+        <label>
+          <input
+            type="checkbox"
+            value="binance"
+            checked={selectedExchanges.includes("binance")}
+            onChange={handleExchangeCheckboxChange}
+            className="mr-2"
+          />
+          Binance
+        </label>
+        <label className="ml-4">
+          <input
+            type="checkbox"
+            value="kraken"
+            checked={selectedExchanges.includes("kraken")}
+            onChange={handleExchangeCheckboxChange}
+            className="mr-2"
+          />
+          Kraken
+        </label>
       </div>
-    </>
+      <div className="flex mb-4">
+        <input
+          type="text"
+          placeholder="Please select a trading pair"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onKeyDown={handleKeyPress}
+          className="border rounded p-2 flex-grow mr-2"
+          list="symbolSuggestions"
+        />
+        <datalist id="symbolSuggestions">
+          {intersectionValues.map((suggestion, index) => (
+            <option key={index} value={suggestion} />
+          ))}
+        </datalist>
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+          onClick={handleSearchClick}
+        >
+          Search
+        </button>
+      </div>
+      {error && <p className="text-center text-red-600">{error}</p>}
+      {selectedExchanges.length === 0 && searching && !error && (
+        <p className="text-center text-red-600">
+          Please select an exchange to view the order book.
+        </p>
+      )}
+      {selectedExchanges.includes("binance") &&
+        !selectedExchanges.includes("kraken") && <Binance />}
+      {selectedExchanges.includes("kraken") &&
+        !selectedExchanges.includes("binance") && <Kraken />}
+      {selectedExchanges.includes("binance") &&
+        selectedExchanges.includes("kraken") && <ConsolidatedOrderBook />}
+    </div>
   );
 };
 
--- src/context/OrderBookContext.js
+++ src/context/OrderBookContext.js
@@ -1,139 +1,231 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useState, useEffect } from "react";
+import axios from "axios";
 
+// Create an OrderBook context
 const OrderBookContext = createContext();
 
+// Create an OrderBookProvider component
 export const OrderBookProvider = ({ children }) => {
   const [krakenBuyers, setKrakenBuyers] = useState([]);
   const [krakenSellers, setKrakenSellers] = useState([]);
   const [binanceBuyers, setBinanceBuyers] = useState([]);
   const [binanceSellers, setBinanceSellers] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const [binanceSocket, setBinanceSocket] = useState(null);
-  const [krakenSocket, setKrakenSocket] = useState(null)
+  const [krakenSocket, setKrakenSocket] = useState(null);
   const [error, setError] = useState(null);
+  const [selectedExchanges, setSelectedExchanges] = useState([]);
 
+  const [isBinanceConnected, setIsBinanceConnected] = useState(false);
+  const [isKrakenConnected, setIsKrakenConnected] = useState(false);
+
+  // Effect to handle search and WebSocket connections
   useEffect(() => {
     if (searchQuery) {
-        setBinanceBuyers([])
-        setBinanceSellers([])
-        setKrakenBuyers([]);
-        setKrakenSellers([]);
-        const formattedQuery = searchQuery.replace('/', '').toLowerCase();
-      connectBinanceSocket(formattedQuery);
-      connectKrakenSocket(searchQuery);
+      setBinanceBuyers([]);
+      setBinanceSellers([]);
+      setKrakenBuyers([]);
+      setKrakenSellers([]);
+      const formattedQuery = searchQuery.replace("/", "").toLowerCase();
+
+      if (isBinanceConnected && selectedExchanges.includes("binance")) {
+        connectBinanceSocket(formattedQuery);
+      }
+      if (isKrakenConnected && selectedExchanges.includes("kraken")) {
+        connectKrakenSocket(searchQuery);
+      }
     } else {
-        setBinanceBuyers([]);
-        setBinanceSellers([]);
-        setKrakenBuyers([]);
-        setKrakenSellers([]);
+      setBinanceBuyers([]);
+      setBinanceSellers([]);
+      setKrakenBuyers([]);
+      setKrakenSellers([]);
       setError(null);
-      if (binanceSocket && krakenSocket) {
+      if (binanceSocket) {
         binanceSocket.close();
-        krakenSocket.close()
+      }
+      if (krakenSocket) {
+        krakenSocket.close();
       }
     }
     return () => {
-        closeBinanceSocket();
-        closeKrakenSocket();
-      };
-  }, [searchQuery]);
+      // Clean up WebSocket connections when unmounting
+      closeBinanceSocket();
+      closeKrakenSocket();
+    };
+  }, [searchQuery, isBinanceConnected, isKrakenConnected]);
 
+  // Function to close WebSocket connections
   const closeBinanceSocket = () => {
     if (binanceSocket) {
       binanceSocket.close();
+      setBinanceSocket(null);
     }
   };
 
+  // Functions to toggle WebSocket connections
+  const toggleBinanceWebSocket = (open) => {
+    setIsBinanceConnected(open);
+    if (!open && binanceSocket) {
+      closeBinanceSocket();
+    }
+  };
+
+  const toggleKrakenWebSocket = (open) => {
+    setIsKrakenConnected(open);
+    if (!open && krakenSocket) {
+      closeKrakenSocket();
+    }
+  };
+
+  // Function to connect to WebSocket for Binance
   const connectBinanceSocket = (pair) => {
     closeBinanceSocket();
 
     const newSocket = new WebSocket(
       `wss://stream.binance.com:9443/ws/${pair.toLowerCase()}@depth10`
     );
-    
+
     newSocket.onopen = () => {
-        setBinanceSocket(newSocket)
-    }
+      setBinanceSocket(newSocket);
+    };
 
     newSocket.onmessage = (event) => {
-        const parsedMessage = JSON.parse(event.data);
+      const parsedMessage = JSON.parse(event.data);
       setBinanceBuyers(parsedMessage.bids);
       setBinanceSellers(parsedMessage.asks);
     };
-
   };
 
   const closeKrakenSocket = () => {
     if (krakenSocket) {
       krakenSocket.close();
+      setKrakenSocket(null);
     }
   };
 
+  // function to connect to websocket for kraken
   const connectKrakenSocket = (pair) => {
-    closeKrakenSocket()
+    closeKrakenSocket();
 
-    const newKrakenSocket = new WebSocket('wss://ws.kraken.com');
+    const newKrakenSocket = new WebSocket("wss://ws.kraken.com");
 
     newKrakenSocket.onopen = () => {
       newKrakenSocket.send(
         JSON.stringify({
-          event: 'subscribe',
+          event: "subscribe",
           pair: [pair.toUpperCase()],
           subscription: {
-            name: 'book',
+            name: "book",
             depth: 100,
           },
         })
       );
-      setKrakenSocket(newKrakenSocket)
+      setKrakenSocket(newKrakenSocket);
     };
 
-    newKrakenSocket.onmessage = event => {
+    newKrakenSocket.onmessage = (event) => {
       const data = JSON.parse(event.data);
-      if (data.event === 'heartbeat') {
+      if (data.event === "heartbeat") {
         return;
       }
 
       if (Array.isArray(data)) {
         const [channelID, updates, channelName, pair] = data;
 
-        if (channelName === 'book-100' && updates) {
+        if (channelName === "book-100" && updates) {
           const updatedBuyers = updates.b || [];
           const updatedSellers = updates.a || [];
 
-          setKrakenBuyers(prevBuyers => updateOrderBook(prevBuyers, updatedBuyers));
-          setKrakenSellers(prevSellers => updateOrderBook(prevSellers, updatedSellers));
-
+          setKrakenBuyers((prevBuyers) =>
+            updateOrderBook(prevBuyers, updatedBuyers)
+          );
+          setKrakenSellers((prevSellers) =>
+            updateOrderBook(prevSellers, updatedSellers)
+          );
         }
-    }
-};
+      }
+    };
   };
 
+  // Function to update the order book
   const updateOrderBook = (prevOrderBook, newEntries) => {
     const updatedOrderBook = [...prevOrderBook];
 
     for (const entry of newEntries) {
       const [price, qty] = entry;
-      const index = updatedOrderBook.findIndex(item => item[0] === price);
+      const index = updatedOrderBook.findIndex((item) => item[0] === price);
 
       if (index !== -1) {
-        if (qty === '0.00000000') {
+        if (qty === "0.00000000") {
           updatedOrderBook.splice(index, 1);
         } else {
           updatedOrderBook[index] = entry;
         }
       } else {
-        if (qty !== '0.00000000') {
+        if (qty !== "0.00000000") {
           updatedOrderBook.push(entry);
         }
       }
     }
 
-    return updatedOrderBook
-      .sort((a, b) => a[0] - b[0])
-      .slice(0, 10);
+    return updatedOrderBook.sort((a, b) => a[0] - b[0]).slice(0, 10);
   };
 
+  console.log("binance", isBinanceConnected);
+  console.log("kraken", isKrakenConnected);
+
+  // Fetch trading symbols and determine the intersection values
+  const [bSymbols, setBsymbols] = useState([]);
+  const [kSymbols, setKsymbols] = useState([]);
+  const [kSymbolsWithoutSlashes, setKsymbolsWithoutSlashes] = useState([]);
+  const [intersectionValues, setIntersectionValues] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const [bSymbolsResponse, kSymbolsResponse] = await Promise.all([
+          axios.get("https://api.binance.com/api/v1/exchangeInfo"),
+          axios.get("https://api.kraken.com/0/public/AssetPairs"),
+        ]);
+
+        const binanceSymbols = bSymbolsResponse.data.symbols.map(
+          (item) => item.symbol
+        );
+        const krakenPairs = Object.keys(kSymbolsResponse.data.result)
+          .filter((symbol) => kSymbolsResponse.data.result[symbol].wsname)
+          .map((symbol) => kSymbolsResponse.data.result[symbol].wsname);
+        const krakenPairsWithoutSlashes = krakenPairs.map((pair) =>
+          pair.replace("/", "")
+        );
+
+        setKsymbols(krakenPairs);
+        setBsymbols(binanceSymbols);
+        setKsymbolsWithoutSlashes(krakenPairsWithoutSlashes);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    // Create the symbol dictionary
+    const dictionary = {};
+    kSymbolsWithoutSlashes.forEach((symbolWithoutSlash, index) => {
+      dictionary[symbolWithoutSlash] = kSymbols[index];
+    });
+
+    const intersectionKeys = Object.keys(dictionary).filter((key) =>
+      bSymbols.includes(key)
+    );
+
+    // Create a new array with the values of the intersection keys
+    const intersectionValues = intersectionKeys.map((key) => dictionary[key]);
+    setIntersectionValues(intersectionValues);
+  }, [kSymbols, bSymbols, kSymbolsWithoutSlashes]);
+
+  // Provide the context values to the components
   return (
     <OrderBookContext.Provider
       value={{
@@ -147,6 +239,15 @@ export const OrderBookProvider = ({ children }) => {
         setBinanceSellers,
         searchQuery,
         setSearchQuery,
+        connectBinanceSocket,
+        connectKrakenSocket,
+        closeBinanceSocket,
+        closeKrakenSocket,
+        toggleBinanceWebSocket,
+        toggleKrakenWebSocket,
+        intersectionValues,
+        selectedExchanges,
+        setSelectedExchanges,
       }}
     >
       {children}
@@ -154,6 +255,7 @@ export const OrderBookProvider = ({ children }) => {
   );
 };
 
+// Create a custom hook to access the OrderBook context
 export const useOrderBook = () => {
   return useContext(OrderBookContext);
 };
